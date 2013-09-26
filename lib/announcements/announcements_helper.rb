5e1dbe295620c728f4f89e2dbe779a9bdaca79e7
Gavin Reynolds <gsreynolds@gmail.com>
2013-09-26 13:56:01 +0100

    Reverse commit 'dafaf8df45515f2621dbacefdd5d8d4c36133dce'

--- lib/announcements/announcements_helper.rb
+++ lib/announcements/announcements_helper.rb
@@ -23,14 +23,14 @@ module AnnouncementsHelper
         close_content_tag = content_tag(:a, text.html_safe, :class => "close", data: data_attribute)
         alert_content_tag = content_tag(:h4, alert_heading, :class => "alert-heading")
 
-        result = content_tag(:div, close_content_tag + alert_content_tag + announcement.body, class: div_class)
+        result = content_tag(:div, close_content_tag + alert_content_tag + announcement.body.html_safe, class: div_class)
       else
         text = options[:hide_message] || "hide message"
         div_class = options[:div_class] || "info"
 
         close_content_tag = content_tag(:span, text, :class => "hide_announcement", data: data_attribute)
 
-        result = content_tag(:div, announcement.body + close_content_tag, class: div_class)
+        result = content_tag(:div, announcement.body.html_safe + close_content_tag, class: div_class)
       end
     end
 
