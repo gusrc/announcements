9be402eaa5392c1f53ca2cabd2a188b46264d7df
Gavin Reynolds <gsreynolds@gmail.com>
2013-09-26 13:10:10 +0100

    Removed attr_accessible, Rails 4 strong parameters changes, don't need mass assignment.

--- lib/generators/announcements/install/templates/announcement.rb
+++ lib/generators/announcements/install/templates/announcement.rb
@@ -3,8 +3,6 @@ class Announcement < ActiveRecord::Base
   before_create :set_default_times
 
   validates_presence_of :body
-
-  attr_accessible :body, :visible_at, :invisible_at
   
   def self.newest
     Announcement.where("visible_at <= ? AND (invisible_at is null OR invisible_at >= ?)", Time.now, Time.now).last
